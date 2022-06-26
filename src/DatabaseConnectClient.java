3906367692efe4104453d49d64e2daee171e6e4c
ATFA <asthingsfallapart@gmail.com>
2022-06-25 21:04:49 -0400

    change window's background color

--- src/DatabaseConnectClient.java
+++ src/DatabaseConnectClient.java
@@ -1,6 +1,3 @@
-import javax.swing.Box;
-import javax.swing.JFrame;
-
 /*
  *  Name: Gregory Flynn
  *  Course: CNT 4714 Summer 2022
@@ -10,6 +7,13 @@ import javax.swing.JFrame;
  *  Class: DatabaseConnectClient.java
  */
 
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 // [FRONT END] [DRIVER]
 // Create a 'DatabaseConnectClient' object that
 //  1. graphically displays all pertinent information
@@ -18,7 +22,10 @@ import javax.swing.JFrame;
 //  4. connects to a specific database via JDBC
 //  5. issues mySQL-style commands
 //  6. logs all client queries and updates
-public class DatabaseConnectClient extends JFrame{
+public class DatabaseConnectClient extends JFrame {
+
+  /* class variables that represent GUI components */
+  private JTextArea queryArea;
   
   /* CONSTRUCTOR */
   public DatabaseConnectClient() {
@@ -26,37 +33,79 @@ public class DatabaseConnectClient extends JFrame{
     // evoke JFrame constructor and provide string.
     // parameter string represents title of application window.
     super("SQL Client App Version 1 - (GDF - CNT 4714 - Summer 2022 - Project 2)");
+    
+    /* initialize GUI components */
+    // try-catch structure necessary because
+    //  1. 'Connector/J' depedency could be missing:
+    //    handled in the 'ClassNotFoundException' catch block
+    //  2. Database connection invalid:
+    //    handled in the 'SQLException' catch block
+    try {
 
-    // create 'Box' object to manage placement of graphical components
-    Box box = Box.createHorizontalBox();
+      /* instantiate a 'JTextArea' object */
+      queryArea = new JTextArea(3, 100);
+      
+      // set text wrapping property.
+      // passing 'true' as a parameter has text wrap at word boundary
+      // passing 'false' has text wrap at character boundary
+      queryArea.setWrapStyleWord(true);
 
-    // place GUI components on content pane
-    add(box);
+      // set line wrapping property.
+      // 'true' paramater has lines wrap if they exceed JTextArea width
+      // 'false' paramter has lines never wrap
+      queryArea.setLineWrap(true);
 
-    // set dimensions of JFrame object
-    setSize(600, 300);
+      // create 'Box' object to manage placement of graphical components
+      Box box = Box.createHorizontalBox();
+      
+      // // place JTextArea object inside of Box object
+      // box.add(queryArea);
 
-    // have the JFrame object create a window
-    setVisible(true);
+      // instantiate a 'Color' object
+      // parameters are (r, g, b) values
+      Color verylightGrey = new Color(238, 238, 238);
 
-    // dispose of window when user quits application.
-    // this is alternative to the default behavior of HIDING on close
-    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+      // change background color of window
+      //    JFrame methods can be called statically since this class
+      //    EXTENDS the JFrame class.
+      //      in other words, the JVM knows JFrame methods are in reference
+      //      to 'DatabaseConnectClient' since this code is inside the class's constructor
+      //      and the class IS A JFrame object.
+      getContentPane().setBackground(verylightGrey);
 
-    // try {
+      // place GUI components on content pane
+      add(queryArea, BorderLayout.NORTH);
 
-    // }
+      // set dimensions of JFrame window
+      setSize(800, 400);
+
+      // have the JFrame object create a window
+      setVisible(true);
+    }
     // catch(ClassNotFoundException classNotFound) {
+      
+    //   // instantiate and intitalize a 'JOptionPane' object
+    //   // to deliver an error pop-up.
+    //   JOptionPane.showMessageDialog(null, "MySQL driver not found",
+    //     "Driver not found", JOptionPane.ERROR_MESSAGE);
 
+    //   // terminate program
+    //   //  the parameter "1" indicates an abnormal program termination
+    //   System.exit(1);
     // }
+    finally {
+      // DISPOSE of window when user quits application.
+      // this is alternative to the default behavior of HIDING on close
+      setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
 
   }
-
+    
   public static void main(String args[]) {
 
     // create a 'DatabaseConnectClient' object which contains all graphical components.
     new DatabaseConnectClient();
 
-    System.out.println("Test");
+    // System.out.println("Test");
   }
 }
