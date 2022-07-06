87ee492f2a6f9723c5f30c6c2959b010166c937d
ATFA <asthingsfallapart@gmail.com>
2022-07-06 00:33:14 -0400

    generate error popups for SQL 'UPDATE' commands

--- src/ResultSetTableModel.java
+++ src/ResultSetTableModel.java
@@ -234,25 +234,30 @@ public class ResultSetTableModel extends AbstractTableModel
 
 
 // set new database update-query string
-   public void setUpdate( String query ) 
+   public void setUpdate( String update ) 
       throws SQLException, IllegalStateException 
    {
-	  int res;
+	   int res;
+
       // ensure database connection is available
       if ( !connectedToDatabase ) 
          throw new IllegalStateException( "Not Connected to Database" );
 
-      // specify query and execute it
-      res = statement.executeUpdate( query );
-/*
-      // obtain meta data for ResultSet
-      metaData = resultSet.getMetaData();
-      // determine number of rows in ResultSet
-      resultSet.last();                   // move to last row
-      numberOfRows = resultSet.getRow();  // get row number      
-*/    
-      // notify JTable that model has changed
-      fireTableStructureChanged();
+      try {
+         // specify query and execute it
+         res = statement.executeUpdate( update );
+
+         // notify JTable that model has changed
+         fireTableStructureChanged();
+      }
+      catch( SQLException sqlException ) {
+         // print debug info to console
+         sqlException.printStackTrace();
+
+         // generate error popup in client
+         JOptionPane.showMessageDialog( null, sqlException.getMessage(),
+            "Database error", JOptionPane.ERROR_MESSAGE );
+      }
    } // end method setUpdate
 
    // close Statement and Connection               
